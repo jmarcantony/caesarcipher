0b1e95bf669678dc5a6fdc908340ba304f040e90
Joseph Marc Antony <31582381+jmarcantony@users.noreply.github.com>
2022-01-24 14:27:44 +0400

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// caesercipher package encodes and decodes strings to and from caesar cipher
-package caesercipher
+// caesarcipher package encodes and decodes strings to and from caesar cipher
+package caesarcipher
